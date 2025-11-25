17c17237fee4243d8124df5522f816b522a4d458
Roysandrone1998 <raysandrone@gmail.com>
2025-11-25 03:55:04 -0300

    ultimo

--- server/src/middleware/db.js
+++ server/src/middleware/db.js
@@ -1,11 +1,11 @@
-import { connectDB } from '../config/db.js';
+  import { connectDB } from '../config/db.js';
 
-export default async function dbMiddleware(req, res, next) {
-  try {
-    await connectDB();
-    next();
-  } catch (err) {
-    console.error('Middleware DB error:', err);
-    res.status(500).json({ error: 'Fallo en la base de datos' });
+  export default async function dbMiddleware(req, res, next) {
+    try {
+      await connectDB();
+      next();
+    } catch (err) {
+      console.error('Middleware DB error:', err);
+      res.status(500).json({ error: 'Fallo en la base de datos' });
+    }
   }
-}
