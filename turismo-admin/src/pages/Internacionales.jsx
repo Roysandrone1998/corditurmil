aee5353f4cfc02a1cd17a174c43946b993302ed4
Roysandrone1998 <raysandrone@gmail.com>
2025-11-25 02:59:45 -0300

    QAZ

--- turismo-admin/src/pages/Internacionales.jsx
+++ turismo-admin/src/pages/Internacionales.jsx
@@ -1,8 +1,8 @@
 import CategoryList from "../components/CategoryList.jsx";
-
+import "../css/internacionales.css";
 export default function Internacionales() {
   return (
-    <main className="page bg-pattern-lg">
+    <main className="page bg-pattern-lg main-internacionales">
       <section className="container my-4">
         <h1 className="text-white font-tommy w-500 text-center ">Viajes Internacionales</h1>
         {/* Pasamos 'internacional' y el CategoryList se encarga de probar plural si hace falta */}
